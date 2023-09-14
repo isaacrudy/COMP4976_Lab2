228541cace6f8243129c47aff652ae9cf0717f8d
isaacrudy <isaac.rudy@gmail.com>
2023-09-14 13:48:19 -0700

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZodiacLab.models;
+
+namespace ZodiacLab.Pages;
+
+public class IndexModel : PageModel
+{
+    public int Year { get; set; }
+    public string Zodiac { get; set; }
+
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        ViewData["Zodiac"] = "";
+        ViewData["ZodiacUrl"] = "";
+    }
+
+    public void OnPost(int year)
+    {
+        if (Request.Form["Clear"].Count > 0)
+        {
+            ViewData["Zodiac"] = "";
+            ViewData["ZodiacUrl"] = "";
+        } else  if (year >= 1900) {
+            ViewData["Zodiac"] = utils.GetZodiac(year);
+            ViewData["ZodiacUrl"] = utils.GetZodiac(year) + ".png";
+        }
+
+    }
+
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+ // Get the current year using JavaScript
+ var currentYear = new Date().getFullYear();
+    
+ // Set the max attribute of the input element to one year from now
+ document.getElementById('year').setAttribute('max', currentYear + 1);
